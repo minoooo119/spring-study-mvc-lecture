c18a243a3db5da8c04a7fb29d2083d7326be7c6d
minoooo119 <minoth0119@naver.com>
2024-07-29 17:30:51 +0900

    [add] HTTP 요청 데이터 -> API 메시지 바디 - 단순 텍스트

---
+++ src/main/java/hello/servlet/basic/request/RequestBodyStringServlet.java
@@ -0,0 +1,30 @@
+package hello.servlet.basic.request;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletInputStream;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.util.StreamUtils;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+@WebServlet(name="requestBodyStringServlet",urlPatterns = "/request-body-string")
+public class RequestBodyStringServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("RequestBodyStringServlet.service");
+        //바로 메시지 바디의 내용을 바이트 코드로 바로 얻을 수 있음 -> string 으로 바꿔야지용
+        ServletInputStream inputStream = request.getInputStream();
+        String messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
+        System.out.println("messageBody = " + messageBody);
+
+        response.getWriter().write("ok");
+        /*
+         * RequestBodyStringServlet.service
+         * messageBody = hello
+         */
+    }
+}
